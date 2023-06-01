69d8e3ff46a5d28d40fc9b1fe6ae96651edb0b99
emin <azamovemin2@gmail.com>
2023-06-01 13:59:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+let arr =
+ [
+    [
+        { a: { price: 20 }  }
+    ],
+      [
+        { a: { price: 35 } }
+      ],
+        [
+         { a: { price: 44 } }
+        ]
+]
+let total =  0
+
+let a   = [{a:20}];
+let b = [{a:35}] 
+let c = [{a:44}]
+console.log(
+    a[0].a + b[0].a + c[0].a
+);
+
